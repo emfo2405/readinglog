b7d7bee898e55390af3675c4781f53e823a280d9
emfo2405 <emfo2405@student.miun.se>
2026-02-26 14:25:28 +0100

    Inställningar för JWT tillagda i settings

--- readinglog/settings.py
+++ readinglog/settings.py
@@ -118,3 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+#Inställningar för JWT med REST Framework
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
