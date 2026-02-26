92191117f68538934951175be18ac09134fa0e0b
emfo2405 <emfo2405@student.miun.se>
2026-02-26 14:41:55 +0100

    Modell skapad för recension

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Modell för bok 
 class Book(models.Model):
@@ -13,6 +14,15 @@ class Book(models.Model):
         return self.title
 
 # Modell för recension
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    content = models.TextField(blank=True, null=True)
+    review = models.IntegerField(blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.book.title}"
 
 
 # Modell för lässtatus
