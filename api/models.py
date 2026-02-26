b075287bc093c15f457ac0283c3c80fd9559d260
emfo2405 <emfo2405@student.miun.se>
2026-02-26 14:39:15 +0100

    Modell skapad för bok

--- api/models.py
+++ api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+# Modell för bok 
+class Book(models.Model):
+    google_id = models.CharField(max_length=100, unique=True)
+    title = models.CharField(max_length=100, blank=False, null=False)
+    author = models.CharField(max_length=100, blank=False, null=False)
+    description = models.TextField(blank=True, null=True)
+    image = models.CharField(blank=True, null=True)
+    pages= models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+# Modell för recension
+
+
+# Modell för lässtatus
+
