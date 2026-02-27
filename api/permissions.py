b3c07f7e3c5707d7275c8ecaedcbffdb2512c5c5
emfo2405 <emfo2405@student.miun.se>
2026-02-27 08:12:05 +0100

    *ndrad permission så att admin får tillgång till allt

--- api/permissions.py
+++ api/permissions.py
@@ -3,4 +3,9 @@ from rest_framework import permissions
 # Skapa ett custom permission för att låte en användare ändra på något om hen är ägaren
 class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
+    #Admin har tillgång till allt
+        if request.user.is_staff:
+            return True
+    #En användare ska bara kunna redigera sina egna inlägg
+
         return obj.user == request.user
