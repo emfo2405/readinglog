f4619841b253b6cf98e94d67b8d3fa42a2793110
emfo2405 <emfo2405@student.miun.se>
2026-02-27 07:23:02 +0100

    Views skapade för book, review, readingstatus och user

--- api/views.py
+++ api/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Book, Review, ReadingStatus
+from .serializers import BookSerializer, ReviewSerializer, ReadingStatusSerializer, UserSerializer
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-# Create your views here.
+# View för Book
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+# View för Review
+class ReviewViewSet(ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    # Koppla review till user
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+# View för Readingstatus
+class ReadingStatusViewSet(ModelViewSet):
+    queryset = ReadingStatus.objects.all()
+    serializer_class = ReadingStatusSerializer
+    permission_classes = [IsAuthenticated]
+
+    # Koppla readingstatus till user
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+# View för en lista av Users
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
