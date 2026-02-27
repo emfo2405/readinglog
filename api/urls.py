43c7371d307dc7e2b85b74cccc7d9315757e0d61
emfo2405 <emfo2405@student.miun.se>
2026-02-27 08:06:22 +0100

    URLs uppdaterade med nya views

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import BookViewSet, ReviewViewSet, ReadingStatusViewSet, UserListView
+from .views import BookViewSet, ReviewViewSet, ReadingStatusViewSet, UserListView, RegisterView, ProfileView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
@@ -7,6 +7,8 @@ urlpatterns = [
     path('review/', ReviewViewSet.as_view(), name='review'),
     path('readingstatus/', ReadingStatusViewSet.as_view(), name='readingstatus'),
     path('users/', UserListView.as_view(), name='users'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh')
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('profile/', ProfileView.as_view(), name='profile')
 ]
--- api/views.py
+++ api/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .models import Book, Review, ReadingStatus
-from .serializers import BookSerializer, ReviewSerializer, ReadingStatusSerializer, UserSerializer
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from .serializers import BookSerializer, ReviewSerializer, ReadingStatusSerializer, UserSerializer, RegisterSerializer
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, AllowAny
 from rest_framework import generics
 from django.contrib.auth.models import User
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .permissions import IsOwner
 
 # View för Book
 class BookViewSet(ModelViewSet):
@@ -38,3 +39,17 @@ class UserListView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
+
+# View för att registrera en användare
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
+# View för att visa eller uppdatera sin profil
+class ProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated, IsOwner]
+
+    def get_object(self):
+        return self.request.user
