519a45f882334f4f0d903cf23dc8ca7790928b51
emfo2405 <emfo2405@student.miun.se>
2026-03-04 15:33:45 +0100

    Fel fixade

--- readinglog/settings.py
+++ readinglog/settings.py
@@ -134,10 +134,7 @@ STATIC_URL = 'static/'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.AllowAny',
-    ),
+    )
 }
 
 SIMPLE_JWT = {
