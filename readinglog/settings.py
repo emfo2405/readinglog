51049a76c6404030e836e0e5a7813e1184cfbddc
emfo2405 <emfo2405@student.miun.se>
2026-03-05 13:40:18 +0100

    Livslängden för token ökad

--- readinglog/settings.py
+++ readinglog/settings.py
@@ -138,7 +138,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=20),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
 
