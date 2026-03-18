28f9eca3ce8d74ddb8ab451efc546f43e089b7fe
emfo2405 <emfo2405@student.miun.se>
2026-03-18 14:08:11 +0100

    Ändringar gjorda så att en utloggad användare får läsa data

--- api/permissions.py
+++ api/permissions.py
@@ -1,8 +1,12 @@
 from rest_framework import permissions
+from rest_framework.permissions import SAFE_METHODS
 
 # Skapa ett custom permission för att låte en användare ändra på något om hen är ägaren
 class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
+    #Tillåt läsning för alla
+        if request.method in SAFE_METHODS:
+            return True
     #Admin har tillgång till allt
         if request.user.is_superuser:
             return True
